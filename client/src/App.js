071c117b64750be184aa6290e4fa1fae1d097c0a
wwSchrader <wwschrader@gmail.com>
2017-08-17 00:21:15 -0700

    Add Navigation Bar

--- client/src/App.js
+++ client/src/App.js
@@ -1,42 +1,13 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import { Route } from 'react-router-dom';
 import './App.css';
+import NavigationBar from './NavigationBar.js';
 
 class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      statusText: "!!!Not Connected to Express!!!"
-    };
-  }
-
-  componentDidMount() {
-    fetch("/api/index", {
-      method: 'get'
-    })
-    .then(response => {
-      if (response.ok) {
-        return response.json();
-      }
-    })
-    .then(resp => {
-      this.setState({
-        statusText: resp.response
-      });
-    })
-    .catch(ex => console.log ("Server fetch went wrong: " + ex));
-  }
-
   render() {
     return (
       <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          {this.state.statusText}
-        </p>
+        <Route path="/" component={NavigationBar} />
       </div>
     );
   }
